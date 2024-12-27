d948cb926d9bf7c5df436dccac998385ae16a3f8
omardiall <omardiallo2005@gmail.com>
2024-12-28 00:42:16 +0100

    last(error)
    last repository bug

---
+++ src/Controller/CommentaireController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use App\Repository\CommentaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentaireController extends AbstractController
+{
+    #[Route('/commentaire', name: 'commentaire_index', methods: ['GET'])]
+    public function index(CommentaireRepository $commentaireRepository): Response
+    {
+        return $this->render('commentaire/index.html.twig', [
+            'commentaires' => $commentaireRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/commentaire/new', name: 'commentaire_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $commentaire = new Commentaire();
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $commentaire->setDate(new \DateTime()); // Set the current date
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('commentaire_index');
+        }
+
+        return $this->render('commentaire/new.html.twig', [
+            'commentaire' => $commentaire,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/commentaire/{id}', name: 'commentaire_show', methods: ['GET'])]
+    public function show(Commentaire $commentaire): Response
+    {
+        return $this->render('commentaire/show.html.twig', [
+            'commentaire' => $commentaire,
+        ]);
+    }
+
+    #[Route('/commentaire/{id}/edit', name: 'commentaire_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Commentaire $commentaire, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('commentaire_index');
+        }
+
+        return $this->render('commentaire/edit.html.twig', [
+            'commentaire' => $commentaire,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/commentaire/{id}', name: 'commentaire_delete', methods: ['POST'])]
+    public function delete(Request $request, Commentaire $commentaire, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$commentaire->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($commentaire);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('commentaire_index');
+    }
+}
---
+++ src/Controller/RecetteController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recette;
+use App\Form\RecetteType;
+use App\Repository\RecetteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecetteController extends AbstractController
+{
+    #[Route('/recette', name: 'recette_index', methods: ['GET'])]
+    public function index(RecetteRepository $recetteRepository): Response
+    {
+        return $this->render('recette/index.html.twig', [
+            'recettes' => $recetteRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/recette/new', name: 'recette_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $recette = new Recette();
+        $form = $this->createForm(RecetteType::class, $recette);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($recette);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('recette_index');
+        }
+
+        return $this->render('recette/new.html.twig', [
+            'recette' => $recette,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/recette/{id}', name: 'recette_show', methods: ['GET'])]
+    public function show(Recette $recette): Response
+    {
+        return $this->render('recette/show.html.twig', [
+            'recette' => $recette,
+        ]);
+    }
+
+    #[Route('/recette/{id}/edit', name: 'recette_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Recette $recette, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(RecetteType::class, $recette);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('recette_index');
+        }
+
+        return $this->render('recette/edit.html.twig', [
+            'recette' => $recette,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/recette/{id}', name: 'recette_delete', methods: ['POST'])]
+    public function delete(Request $request, Recette $recette, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$recette->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($recette);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('recette_index');
+    }
+}
